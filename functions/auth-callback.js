e1933af29d7b2dcae4370aef6b302a9d76d3bfc5
Eric Harris <erich@adobe.com>
2025-05-19 16:21:48 -0400

    Fix for CommonJS.

--- functions/auth-callback.js
+++ functions/auth-callback.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = import("node-fetch");
 
 exports.handler = async ({ queryStringParameters }) => {
     const { code, state } = queryStringParameters;
